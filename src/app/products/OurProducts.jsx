537b7565df1ee01782b47ec9bc7c35862e15f779
Vishal Bhange <vishalbhange333@gmail.com>
2025-04-28 10:01:47 +0530

    responsive

--- src/app/products/OurProducts.jsx
+++ src/app/products/OurProducts.jsx
@@ -41,6 +41,7 @@ const ProductSection = () => {
           </button>
         ))}
       </div>
+      
 
       {/* Product Cards */}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6 px-5">
